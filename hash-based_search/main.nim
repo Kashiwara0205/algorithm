070a646b5ea4f1463cb73841f379324d79c846db
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2020-08-10 15:22:04 +0900

    Implemented hash-based search

---
+++ hash-based_search/main.nim
@@ -0,0 +1,35 @@
+const HASH_TABLE_SIZE = 5
+
+proc hash(value: int): int = abs(value / HASH_TABLE_SIZE).toInt
+
+proc loadTable(size: int, arr: seq[int]): seq[seq[int]] =
+  var hash_table: seq[seq[int]] = newSeq[seq[int]](HASH_TABLE_SIZE)
+
+  for content in arr:
+    let h = hash(content)
+    hash_table[h].add(content)
+
+  return hash_table
+
+proc search(arr: seq[int], t: int): bool =
+  let h = hash(t)
+  let hash_table = loadTable(arr.len, arr)
+  
+  for val in hash_table[h]:
+    if t == val: return true
+
+  return false
+
+block test:
+  var c = @[1, 2, 3]
+  var t = 1
+  assert true == search(c, t)
+
+  c = @[1, 2, 3, 4, 5]
+  t = 10
+  assert false == search(c, t)
+
+  c = @[]
+  t = 1
+  assert false == search(c, t)
+  
