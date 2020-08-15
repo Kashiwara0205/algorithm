cc6a9b17c66bfeca530f886e0e1e2044e1d4efd3
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2020-08-15 11:14:13 +0900

    Add graph obj

---
+++ graph/main.nim
@@ -0,0 +1,52 @@
+type Color* = enum
+  White
+  Gray
+  Black
+
+type Node = ref object
+  number: int
+  pred*: int
+  discovered_cnt*: int
+  finished_cnt*: int
+  color*: Color
+  neighbor*: seq[Node]
+
+proc newNode*(number: int): Node =
+  return Node(number: number, pred: -1, discovered_cnt: -1, 
+              finished_cnt: -1, color: Color.White, neighbor: @[])
+
+type Graph = ref object
+  nodes: seq[Node]
+
+proc getNodes*(graph: Graph): seq[Node] =
+  return graph.nodes
+
+proc newGraph*(nodes: seq[Node]): Graph =
+  return Graph(nodes: nodes)
+
+block test:
+  let node_0 = newNode(0)
+  assert node_0.number == 0
+  assert node_0.pred == -1
+  assert node_0.discovered_cnt == -1
+  assert node_0.finished_cnt == -1
+  assert node_0.color == Color.White
+  assert node_0.neighbor == @[]
+
+  let node_1 = newNode(1)
+  let node_2 = newNode(2)
+
+  # node_0 - > node_1
+  #      |
+  #      - >  node_2
+  node_0.neighbor = @[node_1, node_2]
+  assert node_0.neighbor == @[node_1, node_2]
+
+  # node_0 - > node_1 -
+  #      |             |
+  #      - >  node_2  <- 
+  node_1.neighbor = @[node_2]
+  assert node_1.neighbor == @[node_2]
+
+  let graph = newGraph(@[node_0, node_1, node_2])
+  assert graph.nodes == @[node_0, node_1, node_2]
